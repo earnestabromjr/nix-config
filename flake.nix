7f8b572139843c00cc5470f1b1c97c86e2d28062
earnestabromjr <abromearnest@gmail.com>
2025-01-03 23:51:11 -0600

    Copied from nixos-config

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "A simple example of a test suite";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
+    # hyprland-xdph-git.url = "github:hyprwm/xdg-desktop-portal-hyprland";
+    # hyprland-protocols-git.url = "github:hyprwm/xdg-desktop-portal-hyprland";
+    # # This overrides each input for `hyprland-nix` to use the ones
+    # # specified above, which are locked by you.
+    # hyprland-nix.url = "github:spikespaz/hyprland-nix";
+    # hyprland-nix.inputs = {
+    #     hyprland.follows = "hyprland";
+    #     hyprland-xdph.follows = "hyprland-xdph-git";
+    #     hyprland-protocols.follows = "hyprland-protocols-git";
+    # };
+  };
+
+  outputs = { nixpkgs, home-manager, hyprland, ... } @ inputs:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        inherit system;
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+    homeConfigurations = {
+      terrya = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = 
+        [
+          ./home.nix 
+        ];
+      };
+    };
+  };
+}
